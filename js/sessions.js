c6e53117cd6e8b5e3796463b088e11ff49d0bc46
Richard Hood <rick@flowmediadesign.com>
2018-02-04 10:14:50 -0500

    added sessions js

---
+++ js/sessions.js
@@ -0,0 +1,54 @@
+// see https://stackoverflow.com/a/34908037/5855010
+
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', '../data/sessions.json', true);
+    xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+        }
+    }
+    xobj.send(null);
+}
+
+// Call to function with anonymous callback
+loadJSON(function(response) {
+    data = JSON.parse(response);
+    // loop over the data object
+    for (var key in data) {
+        for (var i = 0; i < data[key].length; i++) {
+            var name = data[key][i].name;
+            var day = data[key][i].day;
+            var start = data[key][i].start;
+            var end = data[key][i].end;
+            var speaker = data[key][i].speaker;
+            var type = data[key][i].type;
+            var room = data[key][i].room;
+            var description = data[key][i].description;
+            var session = document.createElement('div');
+            session.className = 'session';
+            session.innerHTML =
+                '<div class="top">' +
+                '<div class="time">' + start + ' - ' + end + '<img src="images/lil-nerdy.png"></div>' +
+                '<div class="title">' + '<h4 class="name">' + name + '</h4>' + '</div>' +
+                '<h5 class="speaker" >' + speaker + '</h5>' +
+                '<p class="room" >Room: ' + room + ' | ' + type + '</p>' +
+                '</div>' +
+                '<div class="info">' +
+                '<p>' + description + '</p>' +
+                '</div>';
+            // index.html has this:  <div id="sessions"></div>
+            document.getElementById(key).appendChild(session);
+           // $(key).html(session);
+        }
+    }
+});
+
+
+
+
+
+
+
